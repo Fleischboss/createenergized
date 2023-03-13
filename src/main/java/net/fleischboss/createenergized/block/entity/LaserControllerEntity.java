8713724e7d919215ffcfa81f85e5a10291ea33d2
Fleischboss <raphael.wollny@outlook.de>
2023-03-13 17:00:19 +0100

    made laser only work at daytime (placeholder for redstone condition)

--- src/main/java/net/fleischboss/createenergized/block/entity/LaserControllerEntity.java
+++ src/main/java/net/fleischboss/createenergized/block/entity/LaserControllerEntity.java
@@ -3,6 +3,7 @@ package net.fleischboss.createenergized.block.entity;
 import net.fleischboss.createenergized.block.custom.LaserController;
 import net.fleischboss.createenergized.particle.LaserParticleData;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
@@ -15,10 +16,13 @@ public class LaserControllerEntity extends BlockEntity {
     }
 
     public static void tick(Level level, BlockPos pos, BlockState state, LaserControllerEntity entity) {
-        if (!level.isClientSide()) {
-            ServerLevel serverLevel = (ServerLevel) level;
-            for (ServerPlayer serverPlayer : serverLevel.players()) {
-                serverLevel.sendParticles(serverPlayer, new LaserParticleData(state.getValue(LaserController.FACING), 100, 0.3f), true, pos.getX()+0.5, pos.getY()+0.5, pos.getZ()+0.5, 1, 0, 0, 0, 0);
+
+        if (level.isDay()) {
+            if (!level.isClientSide()) {
+                ServerLevel serverLevel = (ServerLevel) level;
+                for (ServerPlayer serverPlayer : serverLevel.players()) {
+                    serverLevel.sendParticles(serverPlayer, new LaserParticleData(state.getValue(LaserController.FACING), 100, 0.3f), true, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1, 0, 0, 0, 0);
+                }
             }
         }
     }
