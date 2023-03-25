df9d83b1a22d986853ef2b2dfa2476ecee1c7510
captanredbeard <captanredbeard2@gmail.com>
2023-03-25 00:02:51 -0500

    More Copying! and Failure!

--- src/main/java/net/fleischboss/createenergized/AllCEBlocks.java
+++ src/main/java/net/fleischboss/createenergized/AllCEBlocks.java
@@ -6,9 +6,15 @@ import com.simibubi.create.foundation.block.BlockStressDefaults;
 import com.simibubi.create.foundation.data.BlockStateGen;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.fleischboss.createenergized.block.custom.LaserController;
+import net.fleischboss.createenergized.item.ModCreativeModeTab;
 import net.fleischboss.createenergized.rotational.components.lazers.EmitterBlock;
 import net.fleischboss.createenergized.rotational.components.lazers.EmitterMovementBehaviour;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.registries.RegistryObject;
 
 import static com.simibubi.create.AllMovementBehaviours.movementBehaviour;
 import static com.simibubi.create.Create.REGISTRATE;
@@ -16,7 +22,8 @@ import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
 
 public class AllCEBlocks {
-    public static final BlockEntry<EmitterBlock> LAZER_EMITTER = REGISTRATE.block("lazer_emitter", EmitterBlock::new)
+    public static final BlockEntry<EmitterBlock> LAZER_EMITTER = REGISTRATE
+            .block("lazer_emitter", EmitterBlock::new)
             .initialProperties(SharedProperties::stone)
             .properties(p -> p.color(MaterialColor.PODZOL))
             .transform(axeOrPickaxe())
