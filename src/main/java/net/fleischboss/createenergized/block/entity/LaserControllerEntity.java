7524088919bb95de7d82e581b7d36d801eaaa994
Fleischboss <raphael.wollny@outlook.de>
2023-05-10 15:19:43 +0200

    The laser is now redstone toggleable yay

--- src/main/java/net/fleischboss/createenergized/block/entity/LaserControllerEntity.java
+++ src/main/java/net/fleischboss/createenergized/block/entity/LaserControllerEntity.java
@@ -4,11 +4,15 @@ import net.fleischboss.createenergized.block.custom.LaserController;
 import net.fleischboss.createenergized.particle.LaserParticleData;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.energy.IEnergyStorage;
 
 public class LaserControllerEntity extends BlockEntity {
     public LaserControllerEntity(BlockPos pos, BlockState state) {
@@ -17,7 +21,7 @@ public class LaserControllerEntity extends BlockEntity {
 
     public static void tick(Level level, BlockPos pos, BlockState state, LaserControllerEntity entity) {
 
-        if (level.isDay()) {
+        if (level.hasNeighborSignal(pos)) {
             if (!level.isClientSide()) {
                 ServerLevel serverLevel = (ServerLevel) level;
                 for (ServerPlayer serverPlayer : serverLevel.players()) {
